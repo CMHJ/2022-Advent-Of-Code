775aea1e738dc948e83709dcf1c4449d9db24d26
CMHJ <55470933+CMHJ@users.noreply.github.com>
2022-12-28 10:54:58 +1100

    complete d2 part 2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -3,6 +3,8 @@ mod rps;
 use std::{process::exit, fs};
 use rps::eval_result;
 
+use crate::rps::eval_result_2;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 1 || args.len() > 2 {
@@ -14,6 +16,7 @@ fn main() {
 
     let rounds: Vec<&str> = contents.split('\n').collect();
     let mut total_score: u64 = 0;
+    let mut total_score_2: u64 = 0;
 
     for round in rounds {
         let choices: Vec<&str> = round.split(' ').collect();
@@ -22,7 +25,11 @@ fn main() {
 
         let score = eval_result(&a, &b);
         total_score += score as u64;
+
+        let score_2 = eval_result_2(&a, &b);
+        total_score_2 += score_2 as u64;
     }
 
     println!("Total score if following strategy: {:?}", total_score);
+    println!("Total score if following actual strategy: {:?}", total_score_2);
 }
--- d2/src/rps.rs
+++ d2/src/rps.rs
@@ -18,6 +18,12 @@ enum RPS {
     Scissors,
 }
 
+pub enum GameResult {
+    Lose,
+    Draw,
+    Win,
+}
+
 fn eval_code(c: &char) -> RPS {
     match c {
         'A' => RPS::Rock,
@@ -30,10 +36,21 @@ fn eval_code(c: &char) -> RPS {
     }
 }
 
-pub enum GameResult {
-    Lose,
-    Draw,
-    Win,
+fn eval_code_2(c: &char) -> GameResult {
+    match c {
+        'X' => GameResult::Lose,
+        'Y' => GameResult::Draw,
+        'Z' => GameResult::Win,
+        _ => panic!("Result code not found"),
+    }
+}
+fn eval_result_choice(a: &GameResult) -> char {
+    match a {
+        GameResult::Lose => 'X',
+        GameResult::Draw => 'Y',
+        GameResult::Win => 'Z',
+        _ => panic!("Result code not found"),
+    }
 }
 
 fn eval_code_score(a: &RPS) -> u8 {
@@ -52,30 +69,66 @@ fn eval_score(res: &GameResult) -> u8 {
     }
 }
 
-pub fn eval_result(a: &char, b: &char) -> u8 {
-    let a_eval = eval_code(&a);
-    let b_eval = eval_code(&b);
+fn eval_predetermined_ending(a: &RPS, res: &GameResult) -> RPS {
+    match a {
+        RPS::Rock => match res {
+            GameResult::Win => RPS::Paper,
+            GameResult::Lose => RPS::Scissors,
+            GameResult::Draw => RPS::Rock,
+        },
+        RPS::Paper => match res {
+            GameResult::Win => RPS::Scissors,
+            GameResult::Lose => RPS::Rock,
+            GameResult::Draw => RPS::Paper,
+        },
+        RPS::Scissors => match res {
+            GameResult::Win => RPS::Rock,
+            GameResult::Lose => RPS::Paper,
+            GameResult::Draw => RPS::Scissors,
+        }
+    }
+}
 
-    let res = match a_eval {
-        RPS::Rock => match b_eval {
+fn eval_outcome(a: &RPS, b: &RPS) -> GameResult {
+    match a {
+        RPS::Rock => match b {
             RPS::Rock => GameResult::Draw,
             RPS::Paper => GameResult::Win,
             RPS::Scissors => GameResult::Lose,
         },
-        RPS::Paper => match b_eval {
+        RPS::Paper => match b {
             RPS::Rock => GameResult::Lose,
             RPS::Paper => GameResult::Draw,
             RPS::Scissors => GameResult::Win,
         },
-        RPS::Scissors => match b_eval {
+        RPS::Scissors => match b {
             RPS::Rock => GameResult::Win,
             RPS::Paper => GameResult::Lose,
             RPS::Scissors => GameResult::Draw,
         }
-    };
+    }
+}
+
+pub fn eval_result(a: &char, b: &char) -> u8 {
+    let a_eval = eval_code(&a);
+    let b_eval = eval_code(&b);
+
+    let res = eval_outcome(&a_eval, &b_eval);
 
     let res_score = eval_score(&res);
     let choice_score = eval_code_score(&b_eval);
+
+    res_score + choice_score
+}
+
+pub fn eval_result_2(a: &char, b: &char) -> u8 {
+    let a_eval = eval_code(&a);
+    let b_eval = eval_code_2(&b);
+    let b_choice = eval_predetermined_ending(&a_eval, &b_eval);
+    let res = eval_outcome(&a_eval, &b_choice);
+
+    let res_score = eval_score(&res);
+    let choice_score = eval_code_score(&b_choice);
 
     res_score + choice_score
 }

b10477d35d3c1e91b0854bf7be488f944623fe48
CMHJ <55470933+CMHJ@users.noreply.github.com>
2023-01-31 22:02:35 +1100

    added setup for d6

---
+++ d6/src/main.rs
@@ -0,0 +1,13 @@
+use d6::*;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let path = args.last().unwrap();
+    let input = std::fs::read_to_string(path).unwrap();
+
+    // let msg = solve_part_1(input.clone());
+    // println!("Part 1: Message is {}", msg);
+
+    // let msg = solve_part_2(input);
+    // println!("Part 2: Message is {}", msg);
+}
---
+++ d6/src/test.rs
@@ -0,0 +1,11 @@
+use crate::*;
+
+const TEST_INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+#[test]
+fn test_part_1() {
+}
+
+#[test]
+fn test_part_2() {
+}

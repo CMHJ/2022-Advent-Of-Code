a3518b36a638e03c1dc5cb58fe2ace4642010f38
CMHJ <55470933+CMHJ@users.noreply.github.com>
2023-01-31 21:00:13 +1100

    added stack parsing for d5

--- d5/src/lib.rs
+++ d5/src/lib.rs
@@ -10,26 +10,59 @@ struct Move {
 }
 
 fn parse_input(input: String) {
+    // Create regex for matching groups of numbers
     let regex_num = Regex::new(r"[0-9]+").expect("Could not create regex");
 
     // Split based on double newline
-    // First is cargo stack visualisation
-    // Second is moves to make
+    // First part is cargo stack visualisation
+    // Second part is list of moves to make
     let mut parts: Vec<&str> = input.split("\n\n").collect();
+    // println!("{:?}", &parts);
     let mut stack_visualisation_raw: Vec<&str> = parts.remove(0).lines().collect();
-    println!("{:?}", stack_visualisation_raw);
-    let column_numbers_raw = String::from(stack_visualisation_raw.pop().unwrap());
+    let rearrangement_procedure_raw = parts;
+    // println!("{:?}", stack_visualisation_raw);
 
     // Find number of columns
+    let column_numbers_raw = String::from(stack_visualisation_raw.pop().unwrap());
     let regex_matches: Vec<&str> = regex_num
         .captures_iter(&column_numbers_raw)
         .map(|x| x.get(0).unwrap().as_str())
         .collect();
-    let columns = regex_matches.len();
-    for m in regex_matches {
-        println!("{}", m);
+    let num_columns = regex_matches.len();
+
+    // Create variable for storing parsed stack information
+    let mut stack_list: Vec<Vec<char>> = Vec::with_capacity(num_columns);
+    for _ in 0..num_columns {
+        stack_list.push(Vec::new());
     }
-    println!("Total {}", columns);
+    assert!(stack_list.len() == num_columns);
+
+    // Parse stack visualisation into stack_list which is variable array of
+    // stacks, both of which will be type Vector, just a conceptual
+    // consideration
+    for row in stack_visualisation_raw {
+        // Column text is always spaced the same so just iterate start index
+        // at 1 and add 4 to check for another column
+        let mut i: usize = 0;
+        let mut n: usize = 1;
+        while let Some(c) = row.chars().nth(n) {
+            if c >= 'A' && c <= 'Z' {
+                if let Some(v) = stack_list.get_mut(i) {
+                    v.push(c);
+                }
+            }
+
+            i += 1; // Increment list index
+            n += 4; // Increment character check index
+        }
+    }
+
+    // Debug
+    // for m in regex_matches {
+    //     println!("{}", m);
+    // }
+    println!("Total {}", num_columns);
+    println!("Stack List:\n{:#?}", stack_list);
 
     // return TBD
 }
--- d5/src/main.rs
+++ d5/src/main.rs
@@ -1,5 +1,10 @@
 use d5::*;
 
 fn main() {
-    solve_part_1(input)
+    let args: Vec<String> = std::env::args().collect();
+    let path = args.last().unwrap();
+    let input = std::fs::read_to_string(path).unwrap();
+
+    let score = solve_part_1(input);
+    // println!("Part 1: Number of pairs is {}", score);
 }

ed0f02840f15d88d986f9c243033100510bdcc55
CMHJ <55470933+CMHJ@users.noreply.github.com>
2023-02-04 19:12:01 +1100

    d16 p1 completed

--- d16/src/lib.rs
+++ d16/src/lib.rs
@@ -1,162 +1,198 @@
-use std::collections::HashMap;
+/*!
+D16 - Proboscidea Volcanium - Valve Pressure Problem.
+Answer was taken from https://www.youtube.com/watch?v=bLMj50cpOug as I could
+not work this out myself.
 
-#[derive(Debug)]
+This is a graph problem however it cannot be traversed in the typical way.
+The graph is too large to brute force, and in my first attempt blows the
+stack.  The weights of each edge are changing with each step and needs to be
+considered.  Valves that are blocked and have zero pressure released are of
+no interest.
+
+This graph need to be collapsed to only the nodes that release pressure
+whilst maintaining the time it takes to travel between them.
+
+An algorithm to solve this is the Floyd-Warshall algorithm for finding the
+shortest paths in a directed weighted graph.  This is slightly inefficient
+as we only need to know the shortest path between some of the nodes
+discounting the nodes with 0 flow rate.
+
+FW is O(n^3) but breadth first search is O(V + E) or number of vertices plus
+edges, which apparently can be worse than FW but on average will be better.
+*/
+
+#[cfg(test)]
+mod test;
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::cmp::max;
+
+const TIME_REMAINING_P1_START: usize = 30;
+
+type ValveMap = HashMap<String, Valve>;
+type ValveDistanceMap = HashMap<String, usize>;
+type ValveCache = HashMap<(usize, String, usize), usize>;
+
 struct Valve {
-    flow_rate: usize, // Pressure / minute
+    flow_rate: usize,
     tunnels: Vec<String>,
 }
 
-pub fn solve_d16(input_text: &mut String) -> usize {
-    let valves = parse_input(input_text);
-    let mut scores = solve_path_scores(valves);
-
-    scores.sort_by(|a, b| b.cmp(a));
-    return *scores.first().unwrap();
+struct ValveData {
+    valves: ValveMap,
+    valve_distance_maps: HashMap<String, ValveDistanceMap>,
+    valve_indices: HashMap<String, usize>
+    // valve_cache:
 }
 
-fn parse_input(input: &mut String) -> HashMap<String, Valve> {
-    // Split into valve flow rates and paths
-    // Insert flow rate into hashmap using valve name as key
-    // Insert tunnels into hashmap using valve name as key
-    // Struct containing flowrate and tunnels?
+pub fn solve_p1(input: String) -> usize {
+    let valves: ValveMap = parse_input(input);
+    let valve_data = simplify_graph(valves);
+    let mut valve_cache = ValveCache::new();
+    let max_score = dfs(&valve_data, &mut valve_cache, TIME_REMAINING_P1_START, "AA".to_string(), 0);
 
-    // Delete sections of the line
-    *input = input.replace("Valve ", "");
-    *input = input.replace("has flow rate=", "");
-    *input = input.replace("has flow rate=", "");
-
-    // Initialise HashMap with capacity for each entry
-    let mut valves: HashMap<String, Valve> = HashMap::with_capacity(input.lines().count());
-
-    // Initialise regex for use later
-    let tunnel_pattern =
-        regex::Regex::new(r"tunnels? leads? to valves? ").expect("Could not create regex");
-
-    for line in input.split('\n') {
-        // Split each line based on semi-colon
-        // let mut line_parts: Vec<String> = line.split("; ").map(|s| s.to_string()).collect();
-        let (line_part_1, mut line_part_2) = line
-            .split_once("; ")
-            .map(|(s1, s2)| (s1.to_string(), s2.to_string()))
-            .expect("Could not split line into two strings");
-
-        // let line_part_1 = line_parts.remove(0);
-        // let mut line_part_2 = line_parts.remove(0);
-
-        // Process first part
-        let mut part_1_parts: Vec<String> = line_part_1.split(' ').map(|s| s.to_string()).collect();
-        let valve_name = part_1_parts.remove(0);
-        let valve_flow_rate = part_1_parts.remove(0).parse::<usize>().unwrap();
-        // println!("{} {}", valve_name, valve_flowrate);
-
-        // Process second part
-        // Extract valve tunnels
-        let pattern_matches = tunnel_pattern
-            .captures(&line_part_2)
-            .expect("Found no matches");
-        let pattern_match_location = pattern_matches
-            .get(0)
-            .expect("Unable to retrieve match location");
-        line_part_2.replace_range(
-            pattern_match_location.start()..pattern_match_location.end(),
-            "",
-        );
-
-        // Split by commas
-        let tunnels: Vec<String> = line_part_2.split(", ").map(|s| s.to_string()).collect();
-
-        // Add to HashMap
-        valves.insert(
-            valve_name,
-            Valve {
-                flow_rate: valve_flow_rate,
-                tunnels,
-            },
-        );
-    }
-
-    println!("{:?}", valves);
-    return valves;
+    max_score
 }
 
-fn depth_first_search(
-    valve_name_current: String,
-    path_score_current: usize,
-    minutes_remaining: usize,
-    valves: &HashMap<String, Valve>,
-    valves_opened: Vec<String>,
-    path_scores: &mut Vec<usize>,
-) {
-    let valve_current = valves.get(&valve_name_current).unwrap();
-    let path_score_current = path_score_current;
-    let mut valves_opened = valves_opened;
+fn parse_input(input: String) -> ValveMap {
+    let mut valves: HashMap<String, Valve> = HashMap::new();
+    for line in input.lines() {
+        let valve_name = line
+            .split_ascii_whitespace()
+            .nth(1)
+            .expect("Name parse: Could not get valve name");
+        let valve_name = String::from(valve_name);
+        let flow_rate = line
+            .split(';')
+            .nth(0)
+            .expect("Flowrate parse: could not get 0th part of ; split")
+            .split('=')
+            .nth(1)
+            .expect("Flowrate parse: could not get 1st part of = split")
+            .parse::<usize>()
+            .expect("Flowrate parse: could not parse as usize");
 
-    // Check return condition
-    if minutes_remaining == 0 || valves_opened.len() == valves.len() {
-        path_scores.push(path_score_current);
-        return;
+        let tunnels: Vec<String> = line
+            .split_once("to ")
+            .expect("Tunnel parse: Could not split at \"to \"")
+            .1
+            .split_once(' ')
+            .expect("Tunnel parse: Could not split at spaces")
+            .1
+            .split(", ")
+            .map(|s| String::from(s))
+            .collect();
+
+        // println!("{}, {}, {:?}", valve_name, flow_rate, tunnels);
+        valves.insert(valve_name, Valve { flow_rate, tunnels });
     }
 
-    // Check paths where pipe is opened first
-    if valves_opened.contains(&valve_name_current) == false && valve_current.flow_rate != 0 {
-        valves_opened.push(valve_name_current.clone());
-        let path_score_new = path_score_current + (minutes_remaining - 1) * valve_current.flow_rate;
-        depth_first_search(
-            valve_name_current,
-            path_score_new,
-            minutes_remaining - 2, // 1 for opening, 1 for travelling
-            &valves,
-            valves_opened.clone(),
-            path_scores,
-        )
-    } else {
-        // Check paths by iterating through each neighbour
-        for t in &valve_current.tunnels {
-            depth_first_search(
-                t.clone(),
-                path_score_current,
-                minutes_remaining - 1,
-                &valves,
-                valves_opened.clone(),
-                path_scores,
-            )
+    valves
+}
+
+fn simplify_graph(valves: ValveMap) -> ValveData {
+    /*! Perform BFS to simplify the graph removing valves with a flow rate of 0
+    and keeping track of the total distances between valves of interest.
+    */
+
+    // Map of distance from each 0 flow rate node to every non-0 node.
+    // distance is in minutes
+    let mut valve_distance_maps: HashMap<String, ValveDistanceMap> = HashMap::new();
+    let mut valves_of_interest: Vec<String> = Vec::with_capacity(valves.len());
+
+    // BFS starting from every 0 flow rate valve to build the map
+    for (valve_name, v) in &valves {
+        // Ignore valves with no flowrate but not AA as it is the starting node
+        if valve_name != "AA" && v.flow_rate == 0 {
+            continue;
         }
+
+        if valve_name != "AA" {
+            valves_of_interest.push(valve_name.clone());
+        }
+
+        let valve_map_init = HashMap::from([(valve_name.clone(), 0), ("AA".to_string(), 0)]);
+        valve_distance_maps.insert(valve_name.clone(), valve_map_init);
+
+        let mut visited: HashSet<String> = HashSet::from([valve_name.clone()]);
+        let mut queue: VecDeque<(usize, String)> = VecDeque::from([(0, valve_name.clone())]);
+
+        while let Some((distance, position)) = queue.pop_front() {
+            for neighbour in &valves.get(&position).unwrap().tunnels {
+                // If there is already an entry ignore or mark is visited and continue
+                if visited.contains(neighbour) {
+                    continue;
+                }
+                visited.insert(neighbour.clone());
+
+                // Collect neighbour if flow rate is non-0
+                if valves.get(neighbour).unwrap().flow_rate != 0 {
+                    valve_distance_maps
+                        .entry(valve_name.clone())
+                        .or_insert(HashMap::new())
+                        .insert(neighbour.clone(), distance + 1);
+                }
+
+                // Queue neighbour to be explored + distance to neighbour
+                queue.push_back((distance + 1, neighbour.clone()));
+            }
+        }
+
+        valve_distance_maps.get_mut(valve_name).unwrap().remove(valve_name);
+        if valve_name != "AA" {
+            valve_distance_maps.get_mut(valve_name).unwrap().remove("AA");
+        }
+
+        // println!("{:#?}", valve_distance_maps);
+    }
+
+    // Generate valve indices hashmap to map valve name to position in bitmask
+    let mut valve_indices: HashMap<String, usize> = HashMap::new();
+    for (i, valve_name) in valves_of_interest.iter().enumerate() {
+        valve_indices.insert(valve_name.clone(), i);
+    }
+
+    ValveData {
+        valves,
+        valve_distance_maps,
+        valve_indices
     }
 }
 
-fn solve_path_scores(valves: HashMap<String, Valve>) -> Vec<usize> {
-    let mut path_scores: Vec<usize> = Vec::new();
-    depth_first_search(
-        String::from("AA"),
-        0,
-        30,
-        &valves,
-        Vec::new(),
-        &mut path_scores,
-    );
-
-    // println!("{:?}", path_scores);
-    return path_scores;
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    const INPUT: &str = r#"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-Valve BB has flow rate=13; tunnels lead to valves CC, AA
-Valve CC has flow rate=2; tunnels lead to valves DD, BB
-Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-Valve EE has flow rate=3; tunnels lead to valves FF, DD
-Valve FF has flow rate=0; tunnels lead to valves EE, GG
-Valve GG has flow rate=0; tunnels lead to valves FF, HH
-Valve HH has flow rate=22; tunnel leads to valve GG
-Valve II has flow rate=0; tunnels lead to valves AA, JJ
-Valve JJ has flow rate=21; tunnel leads to valve II"#;
-
-    #[test]
-    fn test_example() {
-        let max_pressure = solve_d16(&mut String::from(INPUT));
-        assert_eq!(1651, max_pressure);
+fn dfs(valve_data: &ValveData, valve_cache: &mut ValveCache, time_remaining: usize, valve_name_current: String, valves_open_bitmask: usize) -> usize {
+    let cache_key = &(time_remaining, valve_name_current.clone(), valves_open_bitmask);
+    if valve_cache.contains_key(cache_key) {
+        return *valve_cache.get(cache_key).unwrap();
     }
+
+    let mut max_score: usize = 0;
+    for neighbour in valve_data.valve_distance_maps.get(&valve_name_current).unwrap().keys() {
+        // Check if valve is already open in the bitmask
+        let bit: usize = 1 << valve_data.valve_indices.get(neighbour).unwrap();
+        if valves_open_bitmask & bit != 0 {
+            continue;
+        }
+
+        // Calculate if time remaining doesn't go past zero
+        let time_remaining_after_move: isize = time_remaining as isize - (1 + valve_data.valve_distance_maps.get(&valve_name_current).unwrap().get(neighbour).unwrap()) as isize;
+        if time_remaining_after_move <= 0 {
+            continue;
+        }
+        let time_remaining_after_move: usize = time_remaining_after_move as usize;
+
+        max_score = max(
+            max_score,
+            dfs(
+                valve_data,
+                valve_cache,
+                time_remaining_after_move,
+                neighbour.clone(),
+                valves_open_bitmask | bit // Open valve in bitmask
+            ) + valve_data.valves.get(neighbour).unwrap().flow_rate * time_remaining_after_move as usize
+        );
+    }
+
+    max_score
 }
--- d16/src/main.rs
+++ d16/src/main.rs
@@ -1,15 +1,10 @@
 use d16::*;
 
 fn main() {
-    let mut args = std::env::args().collect::<Vec<String>>();
-    if args.len() == 1 || args.len() > 2 {
-        eprintln!("Please supply input text file path");
-        std::process::exit(1);
-    }
+    let args: Vec<String> = std::env::args().collect();
+    let path = args.last().unwrap();
+    let input = std::fs::read_to_string(path).unwrap();
 
-    let path = args.remove(0);
-    let mut input = std::fs::read_to_string(path).expect("Unable to read file from supplied path");
-
-    let score_max = solve_d16(&mut input);
-    println!("Max score is: {}", score_max);
+    let score = solve_p1(input.clone());
+    println!("Part 1: Max score is {}", score);
 }

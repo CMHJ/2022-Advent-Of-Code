ad2e8e133941d31e3d37b378e9efd6a5efdbd94b
CMHJ <55470933+CMHJ@users.noreply.github.com>
2023-02-13 21:10:12 +1100

    d9 simplified

--- d9/src/lib.rs
+++ d9/src/lib.rs
@@ -1,11 +1,17 @@
 /*!
 --- Day 9: Rope Bridge ---
+// if abs greater than 1
+// move next link in line with leader link
+// propagate until abs is 1
 
  */
 
 #[cfg(test)]
 mod test;
 
+const KNOTS: usize = 10;
+
+#[derive(Default, Clone, Copy)]
 struct Position {
     x: isize,
     y: isize,
@@ -19,12 +25,6 @@ impl Position {
 
 pub fn solve_p1(input: &str) -> usize {
     //! Returns unique positions Tail has visited
-    let unique_positions = parse_input(input);
-
-    unique_positions.len()
-}
-
-fn parse_input(input: &str) -> Vec<(isize, isize)> {
     let mut unique_positions: Vec<(isize, isize)> = Vec::from([(0, 0)]);
     let mut h = Position { x: 0, y: 0 };
     let mut t = Position { x: 0, y: 0 };
@@ -38,44 +38,89 @@ fn parse_input(input: &str) -> Vec<(isize, isize)> {
             match direction {
                 'L' => {
                     h.x -= 1;
-                    if (h.x - t.x).abs() > 1 {
-                        t.x -= 1;
-                        t.y = h.y;
-                    }
                 }
                 'R' => {
                     h.x += 1;
-                    if (h.x - t.x).abs() > 1 {
-                        t.x += 1;
-                        t.y = h.y;
-                    }
                 }
                 'U' => {
                     h.y += 1;
-                    if (h.y - t.y).abs() > 1 {
-                        t.y += 1;
-                        t.x = h.x;
-                    }
                 }
                 'D' => {
                     h.y -= 1;
-                    if (h.y - t.y).abs() > 1 {
-                        t.y -= 1;
-                        t.x = h.x;
-                    }
                 }
                 _ => {
                     panic!("Unexpected direction!");
                 }
             }
 
+            // Update tail
+            let diff_x = h.x - t.x;
+            let diff_y = h.y - t.y;
+            // If head is moving on x axis and tail needs to update
+            if diff_x.abs() > 1 {
+                if diff_x.is_positive() {
+                    t.x += 1
+                }
+                else {
+                    t.x -= 1
+                }
+                t.y += diff_y;
+            }
+            // If head is moving on y axis and tail needs to update
+            if diff_y.abs() > 1 {
+                if diff_y.is_positive() {
+                    t.y += 1
+                }
+                else {
+                    t.y -= 1
+                }
+                t.x += diff_x;
+            }
+
             if unique_positions.contains(&t.as_tuple()) == false {
                 unique_positions.push(t.as_tuple());
             }
         }
-
     }
 
     // println!("Unique positions: {}", unique_positions.len());
-    unique_positions
+    unique_positions.len()
+}
+
+pub fn solve_p2(input: &str) -> usize {
+    let mut unique_positions: Vec<(isize, isize)> = Vec::from([(0, 0)]);
+    let mut knot_positions: [Position; KNOTS] = [Position::default(); KNOTS];
+
+    // For step in steps
+    for line in input.lines() {
+        let words: Vec<&str> = line.split_whitespace().collect();
+        let direction: char = words.get(0).unwrap().chars().nth(0).unwrap();
+        let steps: usize = words.get(1).unwrap().parse().unwrap();
+
+        // Move head
+        // Calculate head relative to next link
+        // If next link needs to move
+        // Propagate movement until movement condition is no longer satisfied
+        for knot in knot_positions {
+            // println!("{:?}", p.as_tuple());
+        }
+        // Continue
+    }
+
+    0
+}
+
+fn propagate_movement(leader: &Position, follower: &mut Position) {
+    let diff_x = leader.x - follower.x;
+    let diff_y = leader.y - follower.y;
+    if diff_x.abs() > 1 {
+        if diff_x.is_positive() {
+            follower.x = leader.x - 1;
+        }
+        else {
+            follower.x = leader.x + 1;
+        }
+        follower.y = leader.y;
+    }
+    // else if diff_y
 }
--- d9/src/test.rs
+++ d9/src/test.rs
@@ -9,8 +9,29 @@ D 1
 L 5
 R 2";
 
+const TEST_INPUT_2: &str = r"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+
 #[test]
 fn test_p1() {
     let n = solve_p1(TEST_INPUT);
     assert_eq!(n, 13);
+}
+
+#[test]
+fn test_p2_1() {
+    // let n = solve_p2(TEST_INPUT);
+    // assert_eq!(n, 1);
+}
+
+#[test]
+fn test_p2_2() {
+    // let n = solve_p2(TEST_INPUT_2);
+    // assert_eq!(n, 36);
 }
